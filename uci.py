f6deae041a2b2de3855875a9d695f7687c00beb8
lem0nboy <segnalazionidalweb@gmail.com>
2010-06-19 01:30:21 +0200

    Fully reversed the MENU header. Added basic bmp replace functions, not working, i suspect there is a checksum

--- uci.py
+++ uci.py
@@ -3,15 +3,23 @@
 
 import os, sys, struct
 
-"""
-Table len is always 0xBD0 and each record is 0x10 byte wise
-# Record format:
-# 0x00 Bitmap X
-# 0x02 Bitmap Y
-# 0x04 Bitmap width
-# 0x06 Bitmap height
-# 0x08 Unknown (always 5)
-# 0x0C Bitmap offset relative to the end of the table
+""" 
+MENU section format:
+	0x00 MENU magic tag
+	0x20 version
+	0x2C image table start offset
+	0x30 image table entries (0x10 for each entry)
+	0x34 data size (from the position till the end - 8)
+
+Table len is calculated by multiplying the table entries for the record size.
+
+Table record format:
+	0x00 Bitmap X
+	0x02 Bitmap Y
+	0x04 Bitmap width
+	0x06 Bitmap height
+	0x08 Unknown (always 5)
+	0x0C Bitmap offset relative to the end of the table
 """
 
 class uciImage ():
@@ -47,8 +55,10 @@ class uciTheme:
 		self.uciVer  = 'UCI V0.1                        '
 		self.uciDev  = 'YP-Q2                           '
 		
-		if uciFile != None:
-			self.uciHeader = uciFile.read(0x60)
+		self.uciFile = uciFile
+		
+		if self.uciFile != None:
+			self.uciHeader = self.uciFile.read(0x60)
 
 			self.tagsLen = struct.unpack('<I', self.uciHeader[0x1C:0x20])[0]
 			self.fileLen = struct.unpack('<I', self.uciHeader[0x18:0x1C])[0]
@@ -58,35 +68,52 @@ class uciTheme:
 			print 'Tags len 0x%x' % self.tagsLen
 			print 'Unique device id : %s' % self.devCode
 
-			while uciFile.tell() < self.tagsLen:
-				uciTag = uciFile.read(4)
-				uciTagSz = struct.unpack('<I', uciFile.read(4))[0]
-				uciTagFlag = struct.unpack('<H', uciFile.read(2))[0]
-				uciTagPayload = uciFile.read(uciTagSz)
+			while self.uciFile.tell() < self.tagsLen:
+				uciTag = self.uciFile.read(4)
+				uciTagSz = struct.unpack('<I', self.uciFile.read(4))[0]
+				uciTagFlag = struct.unpack('<H', self.uciFile.read(2))[0]
+				uciTagPayload = self.uciFile.read(uciTagSz)
 				
 				self.tags.append({'Name' : uciTag, 'Size' : uciTagSz, 'Flags' : uciTagFlag, 'Payload' : uciTagPayload})
 
-			self.dataHdr = uciFile.read(0x3E)
-			self.uciVer = uciFile.read(0x20)
-			self.uciDev = uciFile.read(0x20)
+			self.dataHdr = self.uciFile.read(0x3E)
+			
+			open('datahdr.bin', 'wb').write(self.dataHdr)
+			
+			self.uciVer = self.uciFile.read(0x20)
+			self.uciDev = self.uciFile.read(0x20)
 
 			print 'Uci version : %s' % self.uciVer
 			print 'Uci for %s' % self.uciDev
 
-			self.shiz = uciFile.read(0x14)
-			self.menuHdr = uciFile.read(0x38)
+			self.shiz = self.uciFile.read(0x14)
+			
+			open('themeshiz.bin', 'wb').write(self.shiz)
+			
+			self.menuHdr = self.uciFile.read(0x38)
 
+			self.menuMagic = self.menuHdr[:0x20]
+			if self.menuMagic[:4] != 'MENU':
+				print 'MENU tag not found'
+				raise valueError
+			else:
+				print 'MENU tag found'
+			self.menuVersion = self.menuHdr[0x20:0x2C]
 			self.tableStart = struct.unpack('<I', self.menuHdr[0x2C:0x30])[0]
+			self.tableEntries = struct.unpack('<I', self.menuHdr[0x30:0x34])[0]
+			self.dataSize = struct.unpack('<I', self.menuHdr[0x34:0x38])[0]
 
-			print 'Table @ 0x%x' % self.tableStart
+			print 'Menu version : %s' % self.menuVersion
+			print 'Table @ 0x%x (%i entries)' % (self.tableStart, self.tableEntries)
+			print 'Menu data len %i' % self.dataSize
 			
 			tabIndex = 0
 			
-			while tabIndex < 0xBD0 / 0x10:
-				uciFile.seek(self.tableStart + (tabIndex * 0x10))
+			while tabIndex < self.tableEntries:
+				self.uciFile.seek(self.tableStart + (tabIndex * 0x10))
 
 				image = uciImage(tabIndex)
-				image.fromTable(uciFile.read(0x10))
+				image.fromTable(self.uciFile.read(0x10))
 				
 				"""print 'Bitmap %i' % tabIndex
 				print 'Bitmap coord -> %i %i' % (ico.bitmapX, ico.bitmapY)
@@ -94,11 +121,11 @@ class uciTheme:
 				print 'Unknown field-> %x' % (ico.unknownField)
 				print 'Bitmap offset-> 0x%x' % ico.bitmapOffset"""
 
-				uciFile.seek(self.tableStart + 0xbd0 + image.bitmapOffset)
+				self.uciFile.seek(self.tableStart + (self.tableEntries * 0x10) + image.bitmapOffset)
 				
-				bmpHdr = uciFile.read(6)
+				bmpHdr = self.uciFile.read(6)
 				bmpSz = struct.unpack('i', bmpHdr[2:])[0] - 6
-				image.setData(bmpHdr + uciFile.read(bmpSz))
+				image.setData(bmpHdr + self.uciFile.read(bmpSz))
 				
 				self.images.append(image)
 
@@ -115,12 +142,19 @@ class uciTheme:
 		
 			if tag['Name'] == 'APIC':
 				print 'Preview JPEG'
-			elif tag['Name'] == 'TCOM':
-				print 'Comment %s' % tag['Payload']
+			elif tag['Name'] == 'TAUT':
+				print 'Author %s' % tag['Payload']
 			elif tag['Name'] == 'TTAG':
-				print 'Tag %s' % tag['Payload']
+				print 'TAG %s' % tag['Payload']				
 			else:
 				print '%s' % hexdump(tag['Payload'], tag['Size'])
+	def updateIconData (self, index, data):
+		self.images[index].data = data
+	def updateIconInFile (self,index):
+		self.uciFile.seek(self.tableStart + (self.tableEntries * 0x10))
+		self.uciFile.seek(self.images[index].bitmapOffset, 1)
+		self.uciFile.write(self.images[index].data)
+		
 		
 def hexdump(src, length = 16): # dumps to a "hex editor" style output
 	result = []
@@ -143,7 +177,7 @@ def hexdump(src, length = 16): # dumps to a "hex editor" style output
 print 'uciExtract Alpha\n(C) 2010 The Lemon Man'
 
 try:
-	uciFile = open(sys.argv[1], 'rb')
+	uciFile = open(sys.argv[1], 'r+b')
 except:
 	print 'Usage:\n\tpython uci.py uciTheme.uci'
 	exit(0)
@@ -156,7 +190,8 @@ except:
 myTheme = uciTheme(uciFile)
 myTheme.dumpImages(sys.argv[1] + '_icons')
 myTheme.dumpTags()
-
+#myTheme.updateIconData(77, open(sys.argv[1] + '_icons/106.bmp', 'rb').read())
+#myTheme.updateIconInFile(77)
 
 
 
